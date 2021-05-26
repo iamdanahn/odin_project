a437b3abb0c4c7d0a23b1950fdc65bae74e44970
Daniel Ahn <iamdanahn@gmail.com>
2021-05-26 14:24:00 -0400

    revise intro project to use express

--- node-intro-project/index.js
+++ node-intro-project/index.js
@@ -1,39 +1,90 @@
-const http = require('http');
+//////////
+// NODE VERSION
+//////////
+
+// const http = require('http');
+// const fs = require('fs');
+
+// http.createServer(function (req, res) {
+//   if (req.url === '/about') {
+//     fs.readFile('about.html', function (err, data) {
+//       if (err) {
+//         res.writeHead(404, {'Content-Type': 'text/html'})
+//         return res.end('404 Content Not Found')
+//       }
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       return res.end()
+//     })
+//   } else if (req.url === '/contact-me') {
+//     fs.readFile('contact-me.html', function (err, data) {
+//       if (err) {
+//         res.writeHead(404, {'Content-Type': 'text/html'})
+//         return res.end('404 Content Not Found')
+//       }
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       return res.end()
+//     })
+//   } else if (req.url === '/') {
+//     fs.readFile('index.html', function (err, data) {
+//       if (err) {
+//         res.writeHead(404, {'Content-Type': 'text/html'})
+//         return res.end('404 Content Not Found')
+//       }
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       return res.end()
+//     })
+//   }
+
+//   res.writeHead(404, {'Content-Type': 'text/html'})
+//   return res.end('404 Content Not Found')
+// }).listen(8080);
+
+//////////
+// EXPRESS VERSION
+//////////
+
+const express = require('express');
+const app = express();
 const fs = require('fs');
 
-http.createServer(function (req, res) {
-  if (req.url === '/about') {
-    fs.readFile('about.html', function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        return res.end('404 Content Not Found')
-      }
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end()
-    })
-  } else if (req.url === '/contact-me') {
-    fs.readFile('contact-me.html', function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        return res.end('404 Content Not Found')
-      }
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end()
-    })
-  } else if (req.url === '/') { 
-    fs.readFile('index.html', function (err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        return res.end('404 Content Not Found')
-      }
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end()
-    })
-  }
+app.use('/', (req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'})
+      return res.end('404 Content Not Found')
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end()
+	})
+})
+app.use('/contact-me', (req, res) => {
+  fs.readFile("contact-me.html", function (err, data) {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("404 Content Not Found");
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
+})
+app.use('/about', (req, res) => {
+  fs.readFile("about.html", function (err, data) {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("404 Content Not Found");
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
+})
 
-  res.writeHead(404, {'Content-Type': 'text/html'})
-  return res.end('404 Content Not Found')
-}).listen(8080);
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`);
+})
